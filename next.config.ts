a89e6cb4d8f302b3038602c5b2034e1c361cb203
Takumi19850822 <kimura@crossgear.jp>
2026-05-13 13:54:53 +0900

    05131354

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    "**/*": ["./node_modules/pg-cloudflare/dist/**", "./node_modules/pg-cloudflare/esm/**"],
+  },
+};
 
 export default nextConfig;
 
